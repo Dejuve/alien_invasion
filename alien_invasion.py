ae2fc5bf3cb2911053b91f4bc376b38208064708
Dejuve <none@aol.com>
2021-12-07 17:07:56 -0500

    Print message added

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,5 +42,5 @@ def run_game():
             gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
             gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
-
+print("Hello")
 run_game()
